6d5ca7175fc037a5ae403b8a572ff643924f2388
justvdlinde <justvdlinde@hotmail.com>
2018-05-29 15:36:33 +0200

    github fuckup fix

--- Assets/Scripts/Eye.cs
+++ Assets/Scripts/Eye.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Eye : Photon.MonoBehaviour {
+public class Eye : Photon.MonoBehaviour
+{
 
     public Transform target;
 
@@ -10,22 +11,27 @@ public class Eye : Photon.MonoBehaviour {
 
     private Vector3 originalRot;
 
-    private void Start() {
+    private void Start()
+    {
         originalRot = transform.eulerAngles;
 
     }
 
-    public void LateUpdate() {
-        if (target != null) {
+    public void LateUpdate()
+    {
+        if (target != null)
+        {
 
             transform.LookAt(target);
             Vector3 rot = transform.eulerAngles;
-            rot.x = Helper.ClampAngle(rot.x, xClamp.min, xClamp.max) ;
-            rot.y = Helper.ClampAngle(rot.y, yClamp.min, yClamp.max) ;
-            rot.z = Helper.ClampAngle(rot.z, zClamp.min, zClamp.max) ;
+            rot.x = Helper.ClampAngle(rot.x, xClamp.min, xClamp.max);
+            rot.y = Helper.ClampAngle(rot.y, yClamp.min, yClamp.max);
+            rot.z = Helper.ClampAngle(rot.z, zClamp.min, zClamp.max);
             transform.eulerAngles = rot;
 
-        } else {
+        }
+        else
+        {
             transform.eulerAngles = originalRot;
         }
     }
@@ -34,22 +40,26 @@ public class Eye : Photon.MonoBehaviour {
     //    target = FindObjectOfType<Player>().transform;
     //}
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         EyeFocusTarget focus = other.GetComponent<EyeFocusTarget>();
-<<<<<<< HEAD
-        if(focus != null) { 
-            if(focus.transform.parent != transform.parent) {
+
+        if (focus != null)
+        {
+            if (focus.transform.parent != transform.parent)
+            {
                 target = focus.transform;
                 Debug.Log("setting target");
                 GetComponent<Collider>().enabled = false;
             }
-=======
-        if(focus.transform.parent != transform.parent) {
-            target = focus.transform;
-            Debug.Log("Found eye follow target");
-            GetComponent<Collider>().enabled = false;
->>>>>>> ecda9c3a2dd530b3fdd2ac27f535cfc4f17f01a9
+
+            if (focus.transform.parent != transform.parent)
+            {
+                target = focus.transform;
+                Debug.Log("Found eye follow target");
+                GetComponent<Collider>().enabled = false;
+
+            }
         }
     }
-
 }
