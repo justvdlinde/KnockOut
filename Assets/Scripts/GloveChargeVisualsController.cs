dd1c70e5ea06b77b10f80dbec86b738c4d22d33f
JelleDekkers <jelle.dekkers@hotmail.com>
2018-06-04 16:13:21 +0200

    Added healthbar back from older version

---
+++ Assets/Scripts/GloveChargeVisualsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveChargeVisualsController : MonoBehaviour {
+
+    public FloatVariable charge;
+    public new Renderer renderer;
+
+    private Material material;
+
+    private void Start() {
+        material = renderer.material;
+    }
+
+    private void Update() {
+        AdjustMaterialTransparency(charge.runTimeValue);
+    }
+
+    private void AdjustMaterialTransparency(float transparency) {
+        Color color = material.color;
+        color.a = (transparency < 1) ?  transparency : 1;
+        material.color = color;
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : Photon.MonoBehaviour {
+
+    public FloatVariable health;
+    public Color fullColor, emptyColor;
+    public bool destroyIfNotMine = true;
+    public new Renderer renderer;
+    public float fullHP;
+    public float emptyHP = 0;
+
+    private float lerp;
+
+    private void Start() {
+        if (destroyIfNotMine && photonView != null && !photonView.isMine)
+            Destroy(gameObject);
+
+        fullHP = health.initialValue;
+    }
+
+    private void Update() {
+        renderer.material.color = Color.Lerp(emptyColor, fullColor, health.runTimeValue / 100);
+    }
+
+    private void OnGUI() {
+        GUI.Label(new Rect(10, 10, 1000, 20), "lerp " + lerp + " hp: " + health.runTimeValue);
+    }
+}
