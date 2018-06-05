5321960f0654803151b051d5e06ebd8e2de46bdf
JelleDekkers <jelle.dekkers@hotmail.com>
2018-06-05 15:00:18 +0200

    Added hitboxes an player scripts to remote and local avatars

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Photon.MonoBehaviour {
 
     public float startingHealth = 100;
     // syncVar?
-    public float healthPoints;
+    public FloatVariable healthPoints;
 
     private void Start() {
-        healthPoints = startingHealth;
+        if(photonView.isMine)
+            healthPoints.runTimeValue = startingHealth;
 
         foreach(HittableLimb limb in GetComponentsInChildren<HittableLimb>()) 
             limb.onHit += ProcessHit;
     }
 
     public void ProcessHit(PunchInfo info, float damage) {
-        healthPoints -= damage;
+        healthPoints.runTimeValue -= damage;
         Debug.Log("damage: " + damage);
-        if (healthPoints <= 0)
+        if (healthPoints.runTimeValue<= 0)
             KnockOut();
     }
 
