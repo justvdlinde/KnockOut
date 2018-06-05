267f6b0509cd33f02267d6b9dd9741dc54cdfa78
justvdlinde <justvdlinde@hotmail.com>
2018-06-05 15:39:52 +0200

    Remove hands + glove texture

---
+++ Assets/Scripts/leftHand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftHand : MonoBehaviour
+{
+
+    private SkinnedMeshRenderer handLeft;
+    private bool hackActivated;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("DeleteAvatar");
+    }
+
+    IEnumerator DeleteAvatar()
+    {
+        yield return new WaitForSeconds(2);
+        handLeft = GameObject.Find("hand_left_renderPart_0").GetComponent<SkinnedMeshRenderer>();
+        hackActivated = true;
+    }
+
+    private void Update()
+    {
+        if (hackActivated)
+        {
+            handLeft.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/rightHand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightHand : MonoBehaviour {
+
+    private SkinnedMeshRenderer handRight;
+    private bool hackActivated;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("DeleteAvatar");
+    }
+
+    IEnumerator DeleteAvatar()
+    {
+        yield return new WaitForSeconds(2);
+        handRight = GameObject.Find("hand_right_renderPart_0").GetComponent<SkinnedMeshRenderer>();
+        hackActivated = true;
+    }
+
+    private void Update()
+    {
+        if (hackActivated)
+        {
+            handRight.enabled = false;
+        }
+    }
+}
