60a2bfb1bd31420d8ffddf61eeaf040fba6cb9ad
JelleDekkers <jelle.dekkers@hotmail.com>
2018-06-07 14:15:47 +0200

    Added haptic feedback to gloves

--- Assets/Scripts/Glove.cs
+++ Assets/Scripts/Glove.cs
@@ -11,6 +11,11 @@ public class Glove : MonoBehaviour {
 
     [SerializeField]
     private OVRInput.Controller m_controller;
+    private OVRHapticsClip clip;
+
+    private void OnEnable() {
+        InitializeOVRHaptics();
+    }
 
     private void OnTriggerEnter(Collider collider) {
         if (!isEnabled)
@@ -24,11 +29,17 @@ public class Glove : MonoBehaviour {
     private void PunchableObjectHit(Collider collider, IPunchable punchableObject) {
         float velocity = CalculateVelocity();
         punchableObject.Hit(new PunchInfo(transform.position, velocity, charge.runTimeValue));
+        Vibrate();
     }
 
     public float CalculateVelocity() {
         return OVRInput.GetLocalControllerVelocity(m_controller).magnitude;
     }
+
+    //public void PlayHaptic() {
+    //    if(m_controller == OVRInput.Controller.LTouch)
+    //    OVRHaptics.LeftChannel.Preempt(hapticsClip);
+    //}
 
     public void AddCharge(float charge) {
         this.charge.runTimeValue += charge;
@@ -45,4 +56,23 @@ public class Glove : MonoBehaviour {
     public void Enable(bool enable) {
         isEnabled = enable;
     }
+
+    private void InitializeOVRHaptics() {
+        int count = 10;
+        clip = new OVRHapticsClip(count);
+
+        for (int i = 0; i < count; i++) {
+            clip.Samples[i] = i % 2 == 0 ? (byte)0 : (byte)180;
+        }
+
+        clip = new OVRHapticsClip(clip.Samples, clip.Samples.Length);
+    }
+
+    public void Vibrate() {
+        var channel = OVRHaptics.RightChannel;
+        if (m_controller == OVRInput.Controller.LTouch)
+            channel = OVRHaptics.LeftChannel;
+        channel.Preempt(clip);
+    }
+
 }
