2398d06cc789c628366971149194e6f163c9b62f
JelleDekkers <jelle.dekkers@hotmail.com>
2018-06-12 17:51:10 +0200

    Added ArcadeSpriteController.cs

---
+++ Assets/Scripts/ArcadeSpriteController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcadeSpriteController : MonoBehaviour {
+
+    public FloatMinMax minMax = new FloatMinMax(.2f, 2f);
+
+    private SpriteRenderer rend;
+
+    private void Start() {
+        rend = GetComponent<SpriteRenderer>();
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait() {
+        float rnd = minMax.GetRandom();
+        yield return new WaitForSeconds(rnd);
+        rend.enabled = !rend.enabled;
+        StartCoroutine(Wait());
+    }
+}
